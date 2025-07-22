c53e422c2c2baf7b388252e6b054b302e5e37878
Michal-Babins <michal.babinski@theiagen.com>
2025-07-22 21:17:04 +0000

    Add Sentry monitoring functionality with SentryMonitor class and init_sentry method

--- src/bioforklift/alerting/__init__.py
+++ src/bioforklift/alerting/__init__.py
@@ -1,3 +1,4 @@
 from .slack import SlackAlert, SlackNotifier, TerraSummary
+from .sentry import SentryMonitor, init_sentry
 
-__all__ = ["SlackAlert", "SlackNotifier", "TerraSummary"]
+__all__ = ["SlackAlert", "SlackNotifier", "TerraSummary", "SentryMonitor", "init_sentry"]
