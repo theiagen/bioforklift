e8584ea45dccd5a8b568084c823a73adc89c8431
Michal-Babins <michal.babinski@theiagen.com>
2025-10-30 18:05:08 +0000

    Set up zach

---
+++ examples/zach_examples/setup.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from bioforklift.bigquery import BigQuery
+
+bq = BigQuery(project="general-theiagen", dataset="automation_test", location="us-central1")
+
+# Create table, commented out after first run
+county_config_table_exists = bq.table_exists("zach_configs")
+if not county_config_table_exists:
+    table_create_res = bq.create_table(
+        table_name="zach_configs", schema_yaml="example_config_schema.yaml"
+    )
+    config_ops = bq.get_config_operations(
+        table_name="zach_configs", config_schema_yaml="example_config_schema.yaml"
+    )
+    new_configs = config_ops.create_config(Path("test_config.json"))
+    print(f"Created new configs: {len(new_configs)}")
+else:
+    print("Table already exists")
+
+samples_table_exists = bq.table_exists("zach_samples")
+if not samples_table_exists:
+    samples_table_create_res = bq.create_table(
+        table_name="zach_samples", schema_yaml="zach_samples_schema.yaml"
+    )
+    print(f"Created samples table: {samples_table_create_res}")
+else:
+    print("Samples table already exists")
