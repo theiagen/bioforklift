cdcc1e1e6d47ffe68c488913358623c41f9e1681
Michal-Babins <michal.babinski@theiagen.com>
2025-10-31 13:01:04 +0000

    Andrew examples

--- examples/andrew_example/terra2bq_example.py
+++ examples/andrew_example/terra2bq_example.py
@@ -1,11 +1,30 @@
+import os
 from pathlib import Path
 from bioforklift.terra2bq import Terra2BQ
+from bioforklift.bigquery import BigQuery
+from bioforklift.alerting import SlackAlert, SlackNotifier, TerraSummary
+
+bq = BigQuery(project="general-theiagen", dataset="automation_test")
+
+# Create table, commented out after first run
+table_exists = bq.table_exists("theron_configs")
+if not table_exists:
+    table_create_res = bq.create_table(
+        table_name="theron_configs", schema_yaml="example_config_schema.yaml"
+    )
+    config_ops = bq.get_config_operations(
+        table_name="theron_configs", config_schema_yaml="example_config_schema.yaml"
+    )
+    new_config = config_ops.create_config("example_configs/test_config.json")
+    print(f"Created new config with ID: {new_config.get('id')}")
+else:
+    print("Table already exists")
 
 terra2bq = Terra2BQ(
         bigquery_project="general-theiagen",
         bigquery_dataset="automation_test",
-        samples_table="andrew_samples",
-        configs_table="andrew_configs",
+        samples_table="theron_samples",
+        configs_table="theron_configs",
         samples_schema_yaml=Path("example_sample_schema.yaml"),
         configs_schema_yaml=Path("example_config_schema.yaml"),
         project_timezone="America/Los_Angeles",
