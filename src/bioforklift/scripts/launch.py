206d4682c6b9e58e909d5337f917423626698801
xonq <konkelzach@tuta.io>
2026-02-03 23:13:12 +0000

    handle configuration and argument parsing defaults

--- src/bioforklift/scripts/launch.py
+++ src/bioforklift/scripts/launch.py
@@ -4,13 +4,12 @@ import argparse
 import logging
 from pathlib import Path
 from datetime import datetime
-from bioforkflift.scripts.configure import CLIConfig
+from bioforklift.scripts.configure import CLIConfig
 from bioforklift.terra import Terra, WorkflowConfig, MethodConfig, MethodRepoMethod
 
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
-
 
 def cl_init():
 
@@ -27,17 +26,18 @@ def launch_args(parser):
     wf_parser = parser.add_argument_group("Workflow Submission Parameters")
     wf_parser.add_argument("-wf", "--workflow", type=str, help="Terra workflow name to run")
     wf_parser.add_argument("-t", "--table", type=str, help="Terra entity table name for workflow")
-    wf_parser.add_argument("-r", "--repository", type=str, help="GitHub repository for workflow source")
-    wf_parser.add_argument("-b", "--branch", type=str, default="main", help="GitHub branch for workflow source; DEFAULT: main")
+    wf_parser.add_argument("-r", "--repository", type=str, help="GitHub repository for workflow source; DEFAULT: github.com/theiagen/public_health_bioinformatics")
+    wf_parser.add_argument("-b", "--branch", type=str, help="GitHub branch for workflow source; DEFAULT: main")
 
     launch_parser = parser.add_argument_group("Workflow Launch Parameters")
+    launch_parser.add_argument("-j", "--json", type=str, help="Path to workflow submission JSON file")
     launch_parser.add_argument("-s", "--sample_col", type=str, default="samplename", help="Column name in entity table for sample names; DEFAULT: samplename")
     # launch_parser.add_argument("--bioblueprint", type=str, help="Path to Bio-Blueprint JSON file for workflow inputs/outputs")
     launch_parser.add_argument("-i", "--input_json", type=str, help="Path to input JSON file for submission inputs")
     launch_parser.add_argument("-o", "--output_json", type=str, help="Path to output JSON file for submission outputs")
-    launch_parser.add_argument("-cc", "--call_cache", action="store_true", default=False, help="Enable call caching for the workflow submission; DEFAULT: False")
+    launch_parser.add_argument("-cc", "--call_cache", action="store_true", help="Enable call caching for the workflow submission")
     launch_parser.add_argument("-c", "--comment", type=str, default="", help="User comment for the workflow submission")
-    launch_parser.add_argument("-ie", "--ignore_empty", action="store_true", default=False, help="Ignore empty outputs in the workflow submission; DEFAULT: False")
+    launch_parser.add_argument("-ie", "--ignore_empty", action="store_true", help="Ignore empty outputs in the workflow submission")
 
     ws_parser = parser.add_argument_group("Terra Workspace Parameters")
     ws_parser.add_argument("-ws", "--workspace", type=str, help="Terra workspace name")
@@ -69,7 +69,8 @@ def generate_method_config(terra, repo_uri, wf_name, table_name, inputs_dict, ou
     )
     return method_config
 
-def launch(args, config=None):
+
+def initialize_config(args, config):
     if not config:
         config = CLIConfig(
             repository=args.repository,
@@ -79,20 +80,62 @@ def launch(args, config=None):
             call_cache=args.call_cache,
             ignore_empty=args.ignore_empty,
         )
+        if not args.branch:
+            config.branch = "main"
     else:
         # Override config values with command-line arguments if provided
         if args.repository is not None:
             config.repository = args.repository
+        elif not config.repository:
+            config.repository = "github.com/theiagen/public_health_bioinformatics"
         if args.workspace is not None:
             config.workspace = args.workspace
         if args.project is not None:
             config.project = args.project
         if args.branch is not None:
             config.branch = args.branch
+        elif not config.branch:
+            config.branch = "main"
         if args.call_cache is not None:
             config.call_cache = args.call_cache
+        elif not config.call_cache:
+            config.call_cache = False
         if args.ignore_empty is not None:
             config.ignore_empty = args.ignore_empty
+        elif not config.ignore_empty:
+            config.ignore_empty = False
+    return config
+
+
+def extract_args_json(args):
+    if args.json:
+        with open(args.json, 'r') as json_file:
+            json_data = json.load(json_file)
+        if not args.workflow:
+            args.workflow = json_data.get("workflow")
+        if not args.table:
+            args.table = json_data.get("table")
+        if not args.input_json:
+            args.input_json = json_data.get("inputs")
+        if not args.output_json:
+            args.output_json = json_data.get("outputs")
+        if not args.sample_col:
+            args.sample_col = json_data.get("sample_col")
+        if not args.comment:
+            args.comment = json_data.get("comment")
+    if args.input_json:
+        with open(args.input_json, 'r') as input_file:
+            args.input_json = json.load(input_file)
+    if args.output_json:
+        with open(args.output_json, 'r') as output_file:
+            args.output_json = json.load(output_file)
+    return args
+
+
+def launch(args, config=None):
+
+    config = initialize_config(args, config)
+    args = extract_args_json(args)
 
     terra = Terra(
         source_workspace=config.workspace,
@@ -108,25 +151,11 @@ def launch(args, config=None):
     if result.ok:
         logger.info(f"{args.table} entity set created successfully")
 
-        # get workflow input parameters from json file
-        if isinstance(input_json, str):
-            with open(input_json, 'r') as input_file:
-                wf_inputs = json.load(input_file)
-        else:
-            wf_inputs = input_json
-
-        # get workflow output parameters from json file
-        if isinstance(output_json, str):
-            with open(output_json, 'r') as output_file:
-                wf_outputs = json.load(output_file)
-        else:
-            wf_outputs = output_json
-
         # get method config dictionary from existing workspace
         if args.preexisting:
             base_method_config_dict = terra.methods.get_method_config(args.workflow, use_destination=True)
         else:
-            base_method_config_dict = generate_method_config(terra, args.workflow, args.table, wf_inputs, wf_outputs, args.branch)
+            base_method_config_dict = generate_method_config(terra, config.repository, args.workflow, args.table, args.input_json, args.output_json, config.branch)
         base_method_config = terra.methods.dict_to_method_config(base_method_config_dict)
 
         mod_method_config = base_method_config.model_copy(deep=True)
@@ -136,15 +165,15 @@ def launch(args, config=None):
         mod_method_config.methodRepoMethod.methodUri = None
         mod_method_config.methodRepoMethod.sourceRepo = "dockstore"
         mod_method_config.methodRepoMethod.methodPath = f"{config.repository}/{args.workflow}"
-        mod_method_config.methodRepoMethod.methodVersion = args.branch
+        mod_method_config.methodRepoMethod.methodVersion = config.branch
         mod_method_config.methodConfigVersion = 0
 
         # set inputs from json file and dynamically set samplename based on table name
-        mod_method_config.inputs = wf_inputs
+        mod_method_config.inputs = args.input_json
         mod_method_config.inputs[f"{args.table}.{args.sample_col}"] = f"this.{args.table}_id"
 
         # set outputs from json file
-        mod_method_config.outputs = wf_outputs
+        mod_method_config.outputs = args.output_json
 
         # Adds or overwrites the method configuration in the Terra workspace
         terra.methods.overwrite_method_config(mod_method_config, use_destination=True)
