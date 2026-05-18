454b0c7a37f9e6bffa5e388aa865388127c79515
xonq <konkelzach@tuta.io>
2026-05-18 18:59:20 +0000

    allow entity_name to be updated

--- src/bioforklift/scripts/launch.py
+++ src/bioforklift/scripts/launch.py
@@ -280,7 +280,9 @@ def prepare_job_dicts(args_dict: dict, config: CLIConfig) -> dict:
             for wf, wf_dict in json_data.items():
                 job_dict[wf] = {}
                 for entity, wf_data in wf_dict.items():
-                    if entity == "null" or entity == "": # artifact of JSON incompatibility with null keys
+                    if args_dict["entity_name"]:
+                        entity = args_dict["entity_name"] 
+                    elif entity == "null" or entity == "": # artifact of JSON incompatibility with null keys
                         entity = None
                     job_dict[wf][entity] = wf_data
                     for arg, require in wf_args.items():
