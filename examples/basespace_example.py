ea80a69d1e6de77158d1e1d0719514291d3a32a7
Theron James <theron.james@theiagen.com>
2026-08-25 22:58:35 +0000

    Remove reference to project/run IDs for `collection_id`

--- examples/basespace_example.py
+++ examples/basespace_example.py
@@ -7,7 +7,7 @@ from pathlib import Path
 def main():
 
     access_token = "abc123"
-    collection_id = "47639625" # "MiSeq: Nextera DNA Flex (replicates of E.coli, B.cereus, and R.sphaeroides)"
+    collection_id = "MiSeq: Nextera DNA Flex (replicates of E.coli, B.cereus, and R.sphaeroides)"
     samples = ['E-coli_1ng_input-rep02_L001', 'R-sphaeroides_100ng_input-rep18_L001', 'R-sphaeroides_1ng_input-rep15_L001']
 
     bs_client = BaseSpaceClient(access_token)
