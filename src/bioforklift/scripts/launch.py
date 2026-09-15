c4f677f55b8cc6e9f7addaece92e0ce1715d7440
xonq <konkelzach@tuta.io>
2026-09-15 18:46:32 +0000

    update space for formatting

--- src/bioforklift/scripts/launch.py
+++ src/bioforklift/scripts/launch.py
@@ -534,11 +534,11 @@ def generate_markdown_log(wf_submissions_log: dict) -> None:
     for wf_name, submissions in wf_submissions_log.items():
         # if there are greater than 1 submission, nest it
         if len(submissions) > 1:
-            output_str += f"- [] {wf_name}\n"
+            output_str += f"- [ ] {wf_name}\n"
             for entity, url in submissions:
-                output_str += f"  - [] [{entity}]({url})\n"
+                output_str += f"  - [ ] [{entity}]({url})\n"
         else:
-            output_str += f"- [] [{wf_name}]({submissions[0][1]})\n"
+            output_str += f"- [ ] [{wf_name}]({submissions[0][1]})\n"
     print(f"Markdown checklist:\n\n{output_str}\n")
 
 
